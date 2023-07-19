842bf31f604e3189d154dae008bfc36e3bb879c3
Vladimir V. Bychkov <github@bychkov.name>
2023-07-19 12:19:59 +0200

    namespaces changed jakarta -> javax

--- src/main/java/org/primefaces/showcase/view/data/MultiScheduleView.java
+++ src/main/java/org/primefaces/showcase/view/data/MultiScheduleView.java
@@ -4,11 +4,11 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.faces.application.FacesMessage;
-import jakarta.faces.context.FacesContext;
-import jakarta.faces.view.ViewScoped;
-import jakarta.inject.Named;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
 
 import org.primefaces.event.schedule.ScheduleEntryMoveEvent;
 import org.primefaces.event.schedule.ScheduleEntryResizeEvent;
